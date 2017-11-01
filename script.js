fe78796c4be39216e8b6cd2f4a24dbc22b17d58c
Dharmesh <dsujeeun@gmail.com>
2017-11-01 15:51:36 -0400

    Add functionality to play game. Styles not added yet.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// player names and scores
+var p1Name = "";
+var p2Name = "";
+var p1Score = 0;
+var p2Score = 0;
+
+var deck = []; // created when user starts the game
+var roundsRemaining = 26;
+
 $(document).ready(loadIntro);
 
 // load intro (first) page for game
@@ -21,12 +30,6 @@ function loadIntro() {
     var btnGetPlayerInfo = $("#btnGetPlayerInfo");
     btnGetPlayerInfo.click(askForNumPlayers);
 }
-
-// player names and scores
-var p1Name = "";
-var p2Name = "";
-var p1Score = 0;
-var p2Score = 0;
 
 function askForNumPlayers() {
     $("#gameBody").empty();
@@ -59,19 +62,159 @@ function askforPlayerNames(numPlayers) {
         $("#cellP2").append("<input type='text' id='txtP2Name' " +
             "required='required'>");
     }
+    $("#gameBody").append("<p id='blankNames'>Player names cannot be blank.</p>");
+    $("#blankNames").attr("style", "display: none");
     $("#gameBody").append("<button id='btnStartGame'>Start Game!</button>");
     $("#btnStartGame").click(function() {
         p1Name = $("#txtP1Name").val();
         if (p2Name == "") {
             p2Name = $("#txtP2Name").val();
         }
-        // simple test to make sure everything is working fine
-        $("#gameBody").append("<p> Player 1:" + p1Name + "</p>");
-        $("#gameBody").append("<p> Player 2:" + p2Name + "</p>");
+        if (p1Name == "" || p2Name == "") {
+            $("#blankNames").attr("style", "display: block");
+        } else {
+            startGame();
+        }
     });
     $("#gameBody").append("<button id='btnBack'>Back</button>");
     $("#btnBack").click(function() {
-        p2Name = "";
+        resetNames();
         askForNumPlayers();
     });
+}
+
+function startGame() {
+    // only create cards if deck is empty
+    if (deck.length == 0) {
+        createDeck();
+    }
+    shuffleDeck();
+
+    $("#gameBody").empty();
+    $("#gameBody").append("<p id='roundsRemaining'>Rounds left: " +
+        roundsRemaining +"</p>");
+    $("#gameBody").append("<table id='tblGame'></table>");
+    $("#tblGame").append("<tr id='rowNames'></tr>");
+    $("#rowNames").append("<td>" + p1Name + "</td>");
+    $("#rowNames").append("<td>" + p2Name + "</td>");
+    $("#tblGame").append("<tr id='rowScores'></tr>");
+    $("#rowScores").append("<td id='p1Score'>" + p1Score + "</td>");
+    $("#rowScores").append("<td id='p2Score'>" + p2Score + "</td>");
+    $("#tblGame").append("<tr id='rowCards'></tr>");
+    $("#rowCards").append("<td><img id='p1Card' src=''></td>");
+    $("#rowCards").append("<td><img id='p2Card' src=''></td>");
+    $("#gameBody").append("<p id='gameComments'></p>");
+    $("#gameBody").append("<button id='btnNextRound'>Next Round</button>");
+    $("#btnNextRound").click(playOneRound);
+    $("#gameBody").append("<button id='btnRestart'>Restart Game</button>");
+    $("#btnRestart").click(restartGame);
+    $("#gameBody").append("<button id='btnMainMenu'>Main Menu</button>");
+    $("#btnMainMenu").click(function() {
+        resetGameInfo();
+        loadIntro();
+    });
+    // for first round only, because user clicked on 'Start Game' button
+    playOneRound();
+}
+
+function createDeck() {
+    var suits = ["C", "D", "S", "H"];
+    for (var i = 1; i <= 13; ++i) {
+        for (var j = 0; j < suits.length; ++j) {
+            deck.push(i + "_" + suits[j]);
+        }
+    }
+}
+
+function shuffleDeck() {
+    for (var i = 0; i < 52; ++i) {
+        var j = Math.floor((Math.random() * 52));
+        var temp = deck[j];
+        deck[j] = deck[i];
+        deck[i] = temp;
+    }
+}
+
+function playOneRound() {
+    var p1CardIndex = (26 - roundsRemaining) * 2;
+    var p2CardIndex = p1CardIndex + 1;
+    updateCards(deck[p1CardIndex], deck[p2CardIndex]);
+    var roundWinner = updateScores(deck[p1CardIndex], deck[p2CardIndex]);
+    // if last round
+    if (roundsRemaining == 1) {
+        var message;
+        if (p1Score > p2Score) {
+            message = p1Name + " wins!";
+        } else if (p1Score < p2Score) {
+            message = p2Name + " wins!";
+        } else {
+            message = "It's a tie!"
+        }
+        $("#btnNextRound").remove();
+        $("#gameComments").html("Game over! " + message);
+        $("#btnRestart").html("Play again!");
+    } else {
+        if (roundWinner == 1) {
+            message = p1Name + " wins this round!";
+        } else if (roundWinner == 2) {
+            message = p2Name + " wins this round!";
+        } else {
+            message = "This round is a tie!";
+        }
+        $("#gameComments").html(message);
+    }
+    --roundsRemaining;
+    $("#roundsRemaining").html("Rounds left: " + roundsRemaining);
+}
+
+function updateCards(p1Card, p2Card) {
+    $("#p1Card").attr("src", "images/" + p1Card + ".svg");
+    $("#p2Card").attr("src", "images/" + p2Card + ".svg");
+}
+
+function updateScores(p1Card, p2Card) {
+    var roundWinner;
+    var p1Rank = parseInt(p1Card.substring(0, p1Card.indexOf("_")));
+    var p2Rank = parseInt(p2Card.substring(0, p2Card.indexOf("_")));
+    if (p1Rank > p2Rank) {
+        p1Score += 2;
+        roundWinner = 1;
+    } else if (p1Rank < p2Rank) {
+        p2Score += 2;
+        roundWinner = 2;
+    } else {
+        p1Score += 1;
+        p2Score += 1;
+        roundWinner = 3;
+    }
+    $("#p1Score").html(p1Score);
+    $("#p2Score").html(p2Score);
+
+    return roundWinner;
+}
+
+function resetNames() {
+    p1Name = "";
+    p2Name = "";
+}
+
+function resetScores() {
+    p1Score = 0;
+    p2Score = 0;
+}
+
+function resetRoundsRemaining() {
+    roundsRemaining = 26;
+}
+
+function resetGameInfo() {
+    resetNames();
+    resetScores();
+    resetRoundsRemaining();
+}
+
+function restartGame() {
+    resetScores();
+    resetRoundsRemaining();
+    startGame();
 }
