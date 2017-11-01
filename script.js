b59bc21795519abbbae14cff941fb88c81177ab0
Dharmesh <dsujeeun@gmail.com>
2017-11-01 09:25:13 -0400

    Load intro page dynamically, to make web app a fully SPA

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+$(document).ready(loadIntro);
+
+// load intro (first) page for game
+function loadIntro() {
+    $("#gameBody").empty();
+    $("#gameBody").append("<h4>Game Rules</h4>");
+    $("#gameBody").append("<ol id='rulesList'></ol>");
+    $("#rulesList").append("<li>You can play against the computer or" +
+        " another person.</li>");
+    $("#rulesList").append("<li>The game is played with a standard 52-card" +
+        " deck. There are 26 rounds in total. In each round, both players" +
+        " are dealt a card. The player holding the card with the higher" +
+        " rank wins 2 points, and the other player gains 0 points." +
+        " If both players hold two cards having the same rank, then" +
+        " both players are awarded 1 point.</li>");
+    $("#rulesList").append("<li>At the end of the game, the player with the" +
+        " higher score wins. Of course, a tie is also a possibility.</li>");
+    $("#rulesList").append("<li><code>ACE</code> is considered the lowest ranked" +
+        " card in the game, while <code>KING</code> is the highest.</li>");
+    $("#gameBody").append("<button id='btnGetPlayerInfo'>Let's Play!</button>");
+    var btnGetPlayerInfo = $("#btnGetPlayerInfo");
+    btnGetPlayerInfo.click(askForNumPlayers);
+}
+
 // player names and scores
 var p1Name = "";
 var p2Name = "";
 var p1Score = 0;
 var p2Score = 0;
-
-// Get player information
-var btnGetPlayerInfo = $("#btnGetPlayerInfo");
-btnGetPlayerInfo.click(askForNumPlayers);
 
 function askForNumPlayers() {
     $("#gameBody").empty();
@@ -19,9 +39,7 @@ function askForNumPlayers() {
         askforPlayerNames(2);
     });
     $("#gameBody").append("<button id='btnBack'>Back</button>");
-    $("#btnBack").click(function() {
-        location.replace("index.html");
-    });
+    $("#btnBack").click(loadIntro);
 }
 
 function askforPlayerNames(numPlayers) {
@@ -30,7 +48,7 @@ function askforPlayerNames(numPlayers) {
     $("#tblPNames").append("<tr id='rowP1'></tr>");
     $("#tblPNames").append("<tr id='rowP2'></tr>");
     $("#rowP1").append("<td>Player 1: </td>");
-    $("#rowP1").append("<td><input type='text' id='txtP1Name'" +
+    $("#rowP1").append("<td><input type='text' id='txtP1Name' " +
         "required='required'> </td>");
     $("#rowP2").append("<td>Player 2: </td>");
     $("#rowP2").append("<td id='cellP2'></td>");
@@ -38,7 +56,7 @@ function askforPlayerNames(numPlayers) {
         p2Name = "Computer";
         $("#cellP2").html("Computer");
     } else {
-        $("#cellP2").append("<input type='text' id='txtP2Name'" +
+        $("#cellP2").append("<input type='text' id='txtP2Name' " +
             "required='required'>");
     }
     $("#gameBody").append("<button id='btnStartGame'>Start Game!</button>");
